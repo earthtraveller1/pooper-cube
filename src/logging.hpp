3d6984007b3c1b82764ad2c8e554400ca989c621
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-11-22 19:40:16 -0500

    Fixed some compilation errors.

--- src/logging.hpp
+++ src/logging.hpp
@@ -6,21 +6,22 @@
 namespace pooper_cube {
     template <typename string_t, typename... args_t>
     inline auto log_error(const string_t& str, args_t... args) noexcept -> void {
-        fmt::println(fmt::fg(fmt::color::red), "[ERROR]: {}", fmt::format(str, args...));
+        fmt::print(fmt::fg(fmt::color::red), "[ERROR]: {}\n", fmt::format(str, args...));
     }
 
     template <typename string_t, typename... args_t>
     inline auto log_warning(const string_t& str, args_t... args) noexcept -> void {
-        fmt::println(fmt::fg(fmt::color::yellow), "[WARNING]: {}", fmt::format(str, args...));
+        fmt::print(fmt::fg(fmt::color::yellow), "[WARNING]: {}\n", fmt::format(str, args...));
     }
 
     template <typename string_t, typename... args_t>
     inline auto log_info(const string_t& str, args_t... args) noexcept -> void {
-        fmt::println("[INFO]: {}", fmt::format(str, args...));
+        fmt::print("[INFO]: {}\n", fmt::format(str, args...));
     }
+    
     template <typename string_t, typename... args_t>
     inline auto log_verbose(const string_t& str, args_t... args) noexcept -> void {
-        fmt::println(fmt::fg(fmt::color::gray), "[VERBOSE]: {}", fmt::format(str, args...));
+        fmt::print(fmt::fg(fmt::color::gray), "[VERBOSE]: {}\n", fmt::format(str, args...));
     }
 }
 
