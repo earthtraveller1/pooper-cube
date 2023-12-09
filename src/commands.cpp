b3cdf84af187b57343e3570c36594192d73bf9fc
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-12-09 10:44:50 -0500

    Added stuff for working wtih command pools/buffers.

---
+++ src/commands.cpp
@@ -0,0 +1,36 @@
+#include "vulkan-objects.hpp"
+#include <vulkan/vulkan_core.h>
+
+using pooper_cube::command_pool_t;
+
+command_pool_t::command_pool_t(const device_t& p_device, uint32_t p_queue_family_index) : m_device(p_device) {
+    const VkCommandPoolCreateInfo pool_info {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+        .queueFamilyIndex = p_queue_family_index,
+    };
+
+    const auto result = vkCreateCommandPool(m_device, &pool_info, nullptr, &m_pool);
+    if (result != VK_SUCCESS) {
+        throw vulkan_creation_exception_t{result, "command pool"};
+    }
+}
+
+auto command_pool_t::allocate_command_buffer() const -> VkCommandBuffer {
+    const VkCommandBufferAllocateInfo alloc_info {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+        .pNext = nullptr,
+        .commandPool = m_pool,
+        .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+        .commandBufferCount = 1,
+    };
+
+    VkCommandBuffer buffer;
+    const auto result = vkAllocateCommandBuffers(m_device, &alloc_info, &buffer);
+    if (result != VK_SUCCESS) {
+        throw vulkan_creation_exception_t{result, "command buffer"};
+    }
+
+    return buffer;
+}
--- src/vulkan-objects.hpp
+++ src/vulkan-objects.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "common.hpp"
 #include "window.hpp"
 
 namespace pooper_cube {
@@ -91,6 +92,24 @@ namespace pooper_cube {
 
             const device_t& m_device;
     };
+
+    struct command_pool_t {
+        public:
+            explicit command_pool_t(const device_t& device, uint32_t queue_family_index);
+            NO_COPY(command_pool_t);
+
+            operator VkCommandPool() const noexcept { return m_pool; }
+
+            auto allocate_command_buffer() const -> VkCommandBuffer;
+
+            ~command_pool_t() noexcept {
+                vkDestroyCommandPool(m_device, m_pool, nullptr);
+            }
+
+        private:
+            VkCommandPool m_pool;
+            const device_t& m_device;
+   };
 
     struct no_adequate_physical_device_exception_t {};
 
