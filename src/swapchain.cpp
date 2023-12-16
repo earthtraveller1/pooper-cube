9ddcf3d85c70aa6a974142c95422fa533f031d05
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-12-16 15:30:50 -0500

    Fixed an error regarding the subresourceRange.

--- src/swapchain.cpp
+++ src/swapchain.cpp
@@ -144,6 +144,13 @@ swapchain_t::swapchain_t(const window_t& p_window, const physical_device_t& p_ph
                     .g = VK_COMPONENT_SWIZZLE_IDENTITY,
                     .b = VK_COMPONENT_SWIZZLE_IDENTITY,
                     .a = VK_COMPONENT_SWIZZLE_IDENTITY,
+                },
+                .subresourceRange = {
+                    .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                    .baseMipLevel = 0,
+                    .levelCount = 1,
+                    .baseArrayLayer = 0,
+                    .layerCount = 1,
                 }
             };
 
