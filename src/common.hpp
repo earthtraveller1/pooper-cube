ab0d2f41c485fede8befe67528385b22bbcd802b
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-03 18:11:58 -0500

    Added an exception type for generic Vulkan exceptions.

--- src/common.hpp
+++ src/common.hpp
@@ -3,6 +3,11 @@
 // Some stuff that everyone uses.
 
 namespace pooper_cube {
+    struct generic_vulkan_exception_t {
+        VkResult error_code;
+        std::string_view what;
+    };
+
     struct vulkan_creation_exception_t {
         VkResult error_code;
         std::string_view object_name;
