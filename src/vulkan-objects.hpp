a1b5db373ce15d3ab948f81935dfb53294b54079
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-04 16:19:33 -0500

    Fixed the format of the vertex attribute description.

--- src/vulkan-objects.hpp
+++ src/vulkan-objects.hpp
@@ -200,7 +200,7 @@ namespace pooper_cube {
         VkVertexInputAttributeDescription {
             .location = 0,
             .binding = 0,
-            .format = VK_FORMAT_R32G32_SFLOAT,
+            .format = VK_FORMAT_R32G32B32_SFLOAT,
             .offset = offsetof(vertex_t, position),
         },
     };
