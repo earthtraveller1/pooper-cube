0ec52fd729f3fc70896d5e2c3a86e670cebafe08
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-13 17:04:00 -0500

    Made GLM conform to Vulkan's depth thing.

--- src/pch.hpp
+++ src/pch.hpp
@@ -17,6 +17,8 @@
 #include <fmt/format.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
