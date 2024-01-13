2f85e89939946452e7301d99a820f70afe85016c
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-01-12 20:58:26 -0500

    Removed extra whitespace.

--- src/descriptors.cpp
+++ src/descriptors.cpp
@@ -1,7 +1,6 @@
 #include "descriptors.hpp"
 
 namespace pooper_cube {
-
     descriptor_layout_t::descriptor_layout_t(const device_t& p_device, std::span<const VkDescriptorSetLayoutBinding> p_bindings) :
         m_device(p_device)
     {
